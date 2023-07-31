92dc4873766ebd02591fd4e71c5e03508e1bfe2d
geekerstar <247507792@qq.com>
2023-07-31 14:28:19 +0800

    se

---
+++ python_se/dict.py
@@ -0,0 +1,86 @@
+"""
+演示数据容器字典的定义
+"""
+
+# 定义字典
+my_dict1 = {"王力鸿": 99, "周杰轮": 88, "林俊节": 77}
+# 定义空字典
+my_dict2 = {}
+my_dict3 = dict()
+print(f"字典1的内容是：{my_dict1}, 类型：{type(my_dict1)}")
+print(f"字典2的内容是：{my_dict2}, 类型：{type(my_dict2)}")
+print(f"字典3的内容是：{my_dict3}, 类型：{type(my_dict3)}")
+
+# 定义重复Key的字典
+my_dict1 = {"王力鸿": 99, "王力鸿": 88, "林俊节": 77}
+print(f"重复key的字典的内容是：{my_dict1}")
+
+# 从字典中基于Key获取Value
+my_dict1 = {"王力鸿": 99, "周杰轮": 88, "林俊节": 77}
+score = my_dict1["王力鸿"]
+print(f"王力鸿的考试分数是：{score}")
+score = my_dict1["周杰轮"]
+print(f"周杰轮的考试分数是：{score}")
+# 定义嵌套字典
+stu_score_dict = {
+    "王力鸿": {
+        "语文": 77,
+        "数学": 66,
+        "英语": 33
+    }, "周杰轮": {
+        "语文": 88,
+        "数学": 86,
+        "英语": 55
+    }, "林俊节": {
+        "语文": 99,
+        "数学": 96,
+        "英语": 66
+    }
+}
+print(f"学生的考试信息是：{stu_score_dict}")
+
+# 从嵌套字典中获取数据
+# 看一下周杰轮的语文信息
+score = stu_score_dict["周杰轮"]["语文"]
+print(f"周杰轮的语文分数是：{score}")
+score = stu_score_dict["林俊节"]["英语"]
+print(f"林俊节的英语分数是：{score}")
+
+
+"""
+演示字典的常用操作
+"""
+my_dict = {"周杰轮": 99, "林俊节": 88, "张学油": 77}
+# 新增元素
+my_dict["张信哲"] = 66
+print(f"字典经过新增元素后，结果：{my_dict}")
+# 更新元素
+my_dict["周杰轮"] = 33
+print(f"字典经过更新后，结果：{my_dict}")
+# 删除元素
+score = my_dict.pop("周杰轮")
+print(f"字典中被移除了一个元素，结果：{my_dict}, 周杰轮的考试分数是：{score}")
+
+# 清空元素, clear
+my_dict.clear()
+print(f"字典被清空了，内容是：{my_dict}")
+
+# 获取全部的key
+my_dict = {"周杰轮": 99, "林俊节": 88, "张学油": 77}
+keys = my_dict.keys()
+print(f"字典的全部keys是：{keys}")
+# 遍历字典
+# 方式1：通过获取到全部的key来完成遍历
+for key in keys:
+    print(f"字典的key是:{key}")
+    print(f"字典的value是：{my_dict[key]}")
+
+# 方式2：直接对字典进行for循环，每一次循环都是直接得到key
+for key in my_dict:
+    print(f"2字典的key是:{key}")
+    print(f"2字典的value是：{my_dict[key]}")
+
+# 统计字典内的元素数量, len()函数
+num = len(my_dict)
+print(f"字典中的元素数量有：{num}个")
+
